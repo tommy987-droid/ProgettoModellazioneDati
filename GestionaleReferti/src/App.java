2efebbecaccc9dddd445a274de3b0680618f8bb9
Tommaso <73421219+tommy987-droid@users.noreply.github.com>
2026-04-02 18:55:32 +0200

    Scheda tecnica e fine app java

--- GestionaleReferti/src/App.java
+++ GestionaleReferti/src/App.java
@@ -4,27 +4,36 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class App {
-
+    // Variabili di connessione ad db
     static String url = "jdbc:mysql://localhost:8889/";
     static String user = "root";
     static String password = "root";
     static String nomeDB = "ospedale";
 
+    // Metodo per query insert
     public static void insert(String query, ArrayList<String> parameters) {
         try (Connection conn = DriverManager.getConnection(url + nomeDB, user, password);
                 PreparedStatement pstmt = conn.prepareStatement(query);) {
             for (int i = 1; i <= parameters.size(); i++) {
-                pstmt.setString(i, parameters.get(i));
+                pstmt.setString(i, parameters.get(i - 1));
             }
             pstmt.executeUpdate();
         } catch (SQLException e) {
-            System.out.println("Errore");
+            System.out.println("Errore: " + e);
         }
     }
 
-    public static void read(String query) {
+    // Metodo per query select
+    public static void read(String query, ArrayList<String> parameters) {
+
         try (Connection conn = DriverManager.getConnection(url + nomeDB, user, password);
                 PreparedStatement pstmt = conn.prepareStatement(query);) {
+            if (!parameters.isEmpty()) {
+                for (int i = 1; i <= parameters.size(); i++) {
+                    pstmt.setString(i, parameters.get(i - 1));
+                }
+
+            }
             ResultSet rs = pstmt.executeQuery();
             ResultSetMetaData meta = rs.getMetaData();
 
@@ -42,51 +51,142 @@ public class App {
                     if (i < columnCount) {
                         System.out.print(" | ");
                     }
-                }
 
-                // Andiamo a capo alla fine di ogni riga del database
-                System.out.println();
+                    // Andiamo a capo alla fine di ogni riga del database
+                }
+                System.out.println("\n------------");
+
             }
         } catch (SQLException e) {
-            System.out.println("Errore");
+            System.out.println("Errore: " + e);
         }
     }
 
+    // Main programma
     public static void main(String[] args) throws Exception {
         System.out.println("Benvenuto nel programma gestione referti");
         Scanner myScanner = new Scanner(System.in);
         boolean uscita = false;
-        while(uscita == false){
-        System.out.println("------------");
-        System.out.println("Inserisci:\n1 per visualizzare i pazienti" +
-                "\n2 per visualizzare i medici\n3 per visualizzare i referti" +
-                "\n4 per inserire referti\n5 per inserire documenti\n6 per uscire");
+        while (uscita == false) {
+            System.out.println("------------");
+            System.out.println("Inserisci:\n1 per visualizzare i pazienti" +
+                    "\n2 per visualizzare i medici\n3 per visualizzare i referti" +
+                    "\n4 per visualizzare i referti di un singolo paziente\n5 per visualizzare i referti di un singolo medico"
+                    +
+                    "\n6 per inserire referti\n7 per inserire documenti\n8 per uscire");
 
-        
-        
-        String scelta = myScanner.nextLine();
+            String scelta = myScanner.nextLine();
+            System.out.println("------------");
+            String query = "";
 
-        switch (scelta) {
-            case "1":
-                System.out.println("visualizzare i pazienti");
-                break;
-            case "2":
-                System.out.println("visualizzare i medici");
-                break;
-            case "3":
-                System.out.println("visualizzare i referti");
-                break;
-            case "4":
-                System.out.println("per inserire referti");
-                break;
-            case "5":
-                System.out.println("inserire documenti");
-                break;
-            case "6":
-                System.out.println("uscire");
-                uscita = true;
-                break;
-        }
+            switch (scelta) {
+                case "1":
+                    query = "select * from paziente";
+                    read(query, new ArrayList<>());
+
+                    break;
+                    
+                case "2":
+                    query = "select * from medico";
+                    read(query, new ArrayList<>());
+                    break;
+
+                case "3":
+                    query = "SELECT referto.id, referto.id_paziente, paziente.nome, paziente.cognome, " +
+                            "referto.id_medico, medico.nome, medico.cognome, allegati.id, allegati.path, allegati.tipo, "
+                            +
+                            "referto.data_visita, referto.diagnosi " +
+                            "FROM referto LEFT JOIN paziente " +
+                            "on referto.id_paziente = paziente.id " +
+                            "LEFT JOIN medico on referto.id_medico = medico.id " +
+                            "LEFT JOIN allegati on referto.id = allegati.id_referto order by referto.id";
+                    read(query, new ArrayList<>());
+
+                    break;
+
+                case "4":
+
+                    query = "SELECT referto.id, referto.id_paziente, paziente.nome, paziente.cognome, " +
+                            "referto.id_medico, medico.nome, medico.cognome, allegati.id, allegati.path, allegati.tipo, "
+                            +
+                            "referto.data_visita, referto.diagnosi " +
+                            "FROM referto LEFT JOIN paziente " +
+                            "on referto.id_paziente = paziente.id " +
+                            "LEFT JOIN medico on referto.id_medico = medico.id " +
+                            "LEFT JOIN allegati on referto.id = allegati.id_referto " +
+                            "where referto.id_paziente = ? order by referto.id";
+
+                    System.out.println("Inserisci id paziente");
+                    String idPaziente = myScanner.nextLine();
+                    ArrayList<String> parametersP = new ArrayList<>();
+                    parametersP.add(idPaziente);
+                    read(query, parametersP);
+
+                    break;
+
+                case "5":
+                    query = "SELECT referto.id, referto.id_paziente, paziente.nome, paziente.cognome, " +
+                            "referto.id_medico, medico.nome, medico.cognome, allegati.id, allegati.path, allegati.tipo, "
+                            +
+                            "referto.data_visita, referto.diagnosi " +
+                            "FROM referto LEFT JOIN paziente " +
+                            "on referto.id_paziente = paziente.id " +
+                            "LEFT JOIN medico on referto.id_medico = medico.id " +
+                            "LEFT JOIN allegati on referto.id = allegati.id_referto " +
+                            "where referto.id_medico = ? order by referto.id";
+
+                    System.out.println("Inserisci id medico");
+                    String idMedico = myScanner.nextLine();
+                    ArrayList<String> parametersM = new ArrayList<>();
+                    parametersM.add(idMedico);
+                    read(query, parametersM);
+                    break;
+
+                case "6":
+
+                    query = "insert into referto(id_paziente, id_medico, data_visita, diagnosi) values(?,?,?,?)";
+                    System.out.println("Inserisci id paziente");
+                    String idPaz = myScanner.nextLine();
+                    System.out.println("Inserisci id medico");
+                    String idMed = myScanner.nextLine();
+                    System.out.println("Inserisci data visita (formato AAAA-MM-GG)");
+                    String dataVisita = myScanner.nextLine();
+                    System.out.println("Inserisci diagnosi");
+                    String diagnosi = myScanner.nextLine();
+                    ArrayList<String> parametersIR = new ArrayList<>();
+                    parametersIR.add(idPaz);
+                    parametersIR.add(idMed);
+                    parametersIR.add(dataVisita);
+                    parametersIR.add(diagnosi);
+                    insert(query, parametersIR);
+
+                    break;
+
+                case "7":
+                    query = "insert into allegati(id_referto, path, tipo) values(?,?,?)";
+                    System.out.println("Inserisci id referto");
+                    String idRef = myScanner.nextLine();
+                    System.out.println("Inserisci path file");
+                    String path = myScanner.nextLine();
+                    System.out.println("Inserisci tipo (Immagine o Documento o Video)");
+                    String tipo = myScanner.nextLine();
+                    ArrayList<String> parametersAl = new ArrayList<>();
+                    parametersAl.add(idRef);
+                    parametersAl.add(path);
+                    parametersAl.add(tipo);
+
+                    insert(query, parametersAl);
+                    break;
+
+                case "8":
+                    System.out.println("Grazie per aver usato il programma!");
+                    uscita = true;
+                    break;
+
+                default:
+                    System.out.println("Comando non valido!");
+            }
+
         }
 
     }
